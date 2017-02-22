777524564eb214105b0964698fe2affdfc14b961
Cleber Barcelos Costa <barcelos.cbc@gmail.com>
2017-02-22 15:25:45 -0300

    Ajuste na validation exception

--- reuse-service/src/main/java/com/github/earchitecture/reuse/exception/ValidationServiceException.java
+++ reuse-service/src/main/java/com/github/earchitecture/reuse/exception/ValidationServiceException.java
@@ -8,5 +8,49 @@ package com.github.earchitecture.reuse.exception;
  */
 public class ValidationServiceException extends Exception {
   private static final long serialVersionUID = 1L;
+  private String message;
+  private Object[] params;
 
+  /**
+   * Aramagem informações das mensagems.
+   * 
+   * @param menssage
+   *          Chave de mapeamento do propertie.
+   * @param params
+   *          parametros a serem adicionados na mensagem.
+   */
+  public ValidationServiceException(String menssage, Object... params) {
+    this.message = menssage;
+    this.params = params;
+  }
+
+  /**
+   * @return the message
+   */
+  public String getMessage() {
+    return message;
+  }
+
+  /**
+   * @param message
+   *          the message to set
+   */
+  public void setMessage(String message) {
+    this.message = message;
+  }
+
+  /**
+   * @return the params
+   */
+  public Object[] getParams() {
+    return params;
+  }
+
+  /**
+   * @param params
+   *          the params to set
+   */
+  public void setParams(Object[] params) {
+    this.params = params;
+  }
 }
