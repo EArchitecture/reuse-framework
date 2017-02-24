bfa80e17a9a8711017ed6abe8a8075fac2476f07
Cleber Barcelos Costa <barcelos.cbc@gmail.com>
2017-02-24 12:19:48 -0300

    Ajuste na controller

---
+++ reuse-view/src/main/java/com/github/earchitecture/reuse/view/spring/controller/Action.java
@@ -0,0 +1,20 @@
+package com.github.earchitecture.reuse.view.spring.controller;
+
+public enum Action {
+  /**
+   * Ação de criação.
+   */
+  CREATE,
+  /**
+   * Ação de Exclusão.
+   */
+  DELETE,
+  /**
+   * Ação de alteração.
+   */
+  EDIT,
+  /**
+   * Ação de alteração.
+   */
+  LIST;
+}
---
+++ reuse-view/src/main/java/com/github/earchitecture/reuse/view/spring/exeption/ValidationControllerException.java
@@ -0,0 +1,6 @@
+package com.github.earchitecture.reuse.view.spring.exeption;
+
+public class ValidationControllerException extends Exception {
+  private static final long serialVersionUID = 1L;
+
+}
